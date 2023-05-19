23fca4bf17af0a864c66c74f66d92c31aef9ed5d
PornoHup <107538335+PornoHup@users.noreply.github.com>
2023-05-19 13:51:43 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "123456789876:Abkkanbsbaks"
+TOKEN = "5842212257:AAE35LLRmmeB7gJAxcBmduPOehkvH9QzxgA"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
